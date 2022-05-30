0020cc04d2dfafa26c662596162b9a8bfefb4c23
fjccpm <fjccpm@gmail.com>
2022-05-30 16:34:07 -0500

    commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import "../style.css"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={photo_grid} className="hero--photo"/>
+            <img src={photo_grid} className="hero--photo" alt=""/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join Unique interactive activities led by one-of-a-kind-hosts-all without leaving home</p>
         </section>
