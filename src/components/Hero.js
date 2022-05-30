9fe7b10154a744c23f9e214a594dd722f6206685
fjccpm <fjccpm@gmail.com>
2022-05-30 16:39:07 -0500

    loading

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import "../style.css"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={photo_grid} className="hero--photo" alt=""/>
+            <img src={photo_grid} className="hero--photo" alt="Sorry!"/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join Unique interactive activities led by one-of-a-kind-hosts-all without leaving home</p>
         </section>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import "../style.css"
 export default function NavBar() {
     return (
         <nav>
-            <img src={airbnb_logo} className="nav--logo" alt=""/>
+            <img src={airbnb_logo} className="nav--logo" alt="Sorry!"/>
         </nav>
     )
 }
